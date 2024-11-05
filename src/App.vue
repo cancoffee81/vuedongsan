63cda4c0795d17132bbd86908bea5acb793f533e
전원주 <cancoffee81@naver.com>
2024-11-05 20:22:57 +0900

    v-for를 활용한 반복문 연습

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <nav class="menu">
+        <a href="#" v-for="i in menu" :key="i">{{ i }}</a>
+    </nav>    
+    <div v-for="(prd,i) in products" :key="i">
+        <h4>{{ prd }}</h4>
+        <p>{{ price[i] }} 만원</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+        products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+        price : [50, 60, 70],
+        menu : ["Home", "Procudts", "About"],
+    }
+  }
+}
+</script>
+
+<style>
+.menu { background: #006; padding:15px; border-radius: 5px; text-align:center; }
+.menu a { color:#fff; padding:10px; text-decoration: none; }
+</style> 
